c32fc82de40bba6e5de64a233f49545077efcfba
Prisacaru Bogdan-Paul <62438221+BogdanPaul15@users.noreply.github.com>
2023-12-03 18:34:44 +0000

    test

--- scheduler/src/schedulers/round_robin.rs
+++ scheduler/src/schedulers/round_robin.rs
@@ -2,6 +2,7 @@ use std::num::NonZeroUsize;
 
 use crate::{Pid, Process, ProcessState, Scheduler, Syscall, SyscallResult};
 
+#[derive(Debug)]
 pub struct ProcessInfo {
     pid: Pid,
     state: ProcessState,
@@ -125,7 +126,7 @@ impl Scheduler for RoundRobin {
                         running_process.timings.2 += usize::from(self.timeslice) - remaining;
                         self.remaining_running_time = remaining;
                     }
-                    // println!("{:?}", self.ready);
+                    println!("{:?}", self.ready);
                     // Save the remaining time for the running process
                     SyscallResult::Pid(new_pid)
                 }
@@ -155,6 +156,9 @@ impl Scheduler for RoundRobin {
         let mut list: Vec<&dyn Process> = Vec::new();
         for i in &self.ready {
             list.push(i)
+        }
+        if let Some(x) = &self.running_process {
+            list.push(x);
         }
         list
     }
